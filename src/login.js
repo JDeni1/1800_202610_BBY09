0e5de3b30ee4bc1c5e364d186e2e87b626c0df35
Ozgur Yaman <oyaman@my.bcit.ca>
2026-03-03 16:05:42 -0800

    Add signup feature with Firebase Auth

--- src/login.js
+++ src/login.js
@@ -2,12 +2,13 @@ console.log("login.js LOADED");
 
 import { auth } from "./firebaseConfig.js";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const form = document.getElementById("loginForm");
 const msg = document.getElementById("msg");
 
 form.addEventListener("submit", async (e) => {
-  e.preventDefault(); // ✅ sayfanın refresh olmasını engeller
+  e.preventDefault();
 
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value;
@@ -22,4 +23,21 @@ form.addEventListener("submit", async (e) => {
   } catch (err) {
     msg.textContent = `❌ ${err.code}`;
   }
+});
+
+// SIGN UP
+document.getElementById("signupBtn").addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  msg.textContent = "Creating account...";
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    msg.textContent = "✅ User created! You can now log in.";
+  } catch (error) {
+    msg.textContent = `❌ ${error.message}`;
+  }
+
 });
