c55f697ba5c6c5dac9f219e62de57af37dd11b6d
dws09plus <sudarwin3@gmail.com>
2026-03-12 15:15:24 -0700

    modified:   map.html 	modified:   navigation.html 	modified:   src/map.js

--- src/map.js
+++ src/map.js
@@ -1,3 +1,63 @@
+// Check that this is at the top of your JS file
+import maplibregl from "maplibre-gl";
+
+// ------------------------------------------------------------
+// This function takes the hike data and adds green pins to the map.
+// It also stores the hike data in a global variable for later use (e.g., zooming).
+// ------------------------------------------------------------
+async function showHikes(map) {
+    // Fetch hike data from Firestore
+    const snapshot = await getHikes();
+
+    // Loop through each hike document and add a green pin to the map
+    snapshot.forEach(doc => {
+
+        // Store hike data in global variable (array)
+        // for later use (e.g., zooming to all points)
+        appState.hikes.push(doc);  
+
+        // create green pin
+        const el = document.createElement("div");
+        el.style.width = "16px";
+        el.style.height = "16px";
+        el.style.borderRadius = "50%";
+        el.style.backgroundColor = "green";
+        el.style.border = "2px solid white";
+
+        // new layer with markers, add to map
+        new maplibregl.Marker({ element: el })
+            .setLngLat([doc.lng, doc.lat])
+            .addTo(map);
+    });
+}
+
+// Add this to the top of your JS file
+// Database initialized
+import { db } from "./firebaseConfig.js";
+// Functions needed to read from database
+import { collection, getDocs } from "firebase/firestore";
+
+
+// ------------------------------------------------------------
+// This function fetches hike data (converted to JSON)
+// from Firestore and adds green pins to the map.
+// It assumes each hike document has "lat" and "lng" fields.
+// ------------------------------------------------------------
+async function getHikes() {
+
+    // Fetch all documents from the "hikes" collection in Firestore
+    const snapshot = await getDocs(collection(db, "hikes"));
+
+    // Convert Firestore documents to plain JavaScript objects
+    // And returns a new array (list of the documents, json format)
+    // Equivalent to doing this:
+    //   const hikes = [];
+    //   for (const doc of snapshot.docs) {
+    //       hikes.push(doc.data());
+    
+    return snapshot.docs.map(doc => doc.data());
+}
+
 // ------------------------------------------------------------
 // Global variable to store user location, hike data - good practice
 // ------------------------------------------------------------
@@ -119,13 +179,4 @@ new maplibregl.Marker()
   .setLngLat([-123.0016, 49.2532])
   .addTo(map);
 
-
-  /**
-   * Add the bus routes:
-   * 
-   */
-  map.addSource("busRoutes", {
-  type: "geojson",
-  data: "/data/bus_routes.geojson"
-});
 
