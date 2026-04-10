8e07d2e3e97c860e4c29f4e3220bb8c288b97ff0
Jill <jbautista19@my.bcit.ca>
2026-04-10 16:52:09 -0700

    code documentation on vit.config and dockerfile to better understand their use

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 
+//Reads when running "npm run build"
 export default defineConfig({
   publicDir: "public/static",
   build: {
     rollupOptions: {
+      //Defines entry points within HTML Pages
       input: {
         main: "index.html",
         login: "public/login.html",
